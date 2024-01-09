4c5dd7148a1099ec426448a7fa998b087ca8d4f8
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2024-01-09 23:19:42 +0900

    Add links of JSFiddle pages.

--- app/(header)/page.tsx
+++ app/(header)/page.tsx
@@ -6,7 +6,13 @@ export default async function Home() {
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Home</h2>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">home page.</p>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">The following URLs are examples of using JSFiddle to replicate vulnerabilities.</p>
+
+          <h3 className="mb-4 text-2xl tracking-tight font-bold text-gray-900 dark:text-white">CSRF (Cross-Site Request Forgery)</h3>
+          <p className="mb-8 lg:mb-16 font-light text-gray-500 dark:text-gray-400 sm:text-xl"><Link href="https://jsfiddle.net/megmogmog/yxkcqd42/">https://jsfiddle.net/megmogmog/yxkcqd42/</Link></p>
+
+          <h3 className="mb-4 text-2xl tracking-tight font-bold text-gray-900 dark:text-white">Clickjacking</h3>
+          <p className="mb-8 lg:mb-16 font-light text-gray-500 dark:text-gray-400 sm:text-xl"><Link href="https://jsfiddle.net/megmogmog/7w35ry2u/">https://jsfiddle.net/megmogmog/7w35ry2u/</Link></p>
         </div>
       </section>
     </>
